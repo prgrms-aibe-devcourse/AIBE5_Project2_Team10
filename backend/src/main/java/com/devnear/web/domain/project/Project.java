76a6b72168b2bd330277aca52ca7672e7a1d64c4
warcat12 <warcat12@gmail.com>
2026-04-13 16:38:40 +0900

    [fix] #38 피드백 반영 완료

--- backend/src/main/java/com/devnear/web/domain/project/Project.java
+++ backend/src/main/java/com/devnear/web/domain/project/Project.java
@@ -110,6 +110,7 @@ public class Project extends BaseTimeEntity {
         this.status = ProjectStatus.COMPLETED;
     }
 
+    
     public void updateSkills(List<ProjectSkill> newProjectSkills) {
         this.projectSkills.clear();
         this.projectSkills.addAll(newProjectSkills);
